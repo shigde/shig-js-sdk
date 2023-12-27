b2b55d307666a24c5caf3b156637aa101b0c854a
Enrico Schwendig <enrico.schw@gmx.de>
2023-12-27 23:37:51 +0100

    add material css

--- projects/core/src/lib/shig-module.ts
+++ projects/core/src/lib/shig-module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {CommonModule} from '@angular/common';
 import {DeviceSettingsComponent, LobbyComponent, SvgSettingsComponent} from './component';
@@ -22,7 +22,8 @@ import {HttpClientModule} from '@angular/common/http';
         DeviceSettingsComponent,
         LobbyComponent,
         SvgSettingsComponent
-    ]
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ShigModule {
 }
